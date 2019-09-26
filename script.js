21f8284341e531c22fd7eb240e5ae2ca3c6618a6
Jakub Nakielski <you@example.com>
2019-09-26 20:54:10 +0200

    Fix showing and hiding side menu

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const style = document.createElement("style");
-document.head.appendChild(style);
-
 const hamburger = document.getElementById("hamburger");
-// const sideMenu = document.getElementById("sideMenu");
-const sideMenu = document.querySelector(".sideMenu");
+const sideMenu = document.getElementById("sideMenu");
 
 hamburger.addEventListener("click", () => {
   sideMenu.classList.toggle("sideMenu--isActive");
 });
 
-document.querySelector(".header").addEventListener("click", () => {
-  sideMenu.classList.remove("sideMenu--isActive");
+window.addEventListener("click", e => {
+  if (sideMenu.contains(e.target)) {
+  } else if (
+    e.target.classList.value != "menu__hamburgerContainer" &&
+    e.target.classList.value != "menu__hamburger"
+  ) {
+    sideMenu.classList.remove("sideMenu--isActive");
+  }
 });
