117a794e0e1aaf64b71aff9a2617bc5e48811552
Jakub Nakielski <you@example.com>
2019-09-26 17:06:39 +0200

    Side menu done

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ document.head.appendChild(style);
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const menuLink = document.getElementById("menu__link");
+const article = document.querySelector(".article");
+
 hamburger.addEventListener("click", () => {
-  console.log(menu.firstElementChild);
-  menu.classList.toggle("toggleMenu");
-  menu.firstElementChild.classList.toggle("toggleColor");
+  menu.classList.toggle("sideMenu--isActive");
 });
+
+// console.log(article);
+
+// article.addEventListener("click", () => {
+//   menu.classList.remove("toggleMenu");
+// });
